612ff77ae85c35ca1fcc77bbc8e3e5e18659a8d2
Thảo <nguyenthao201201@gmail.com>
2023-11-23 18:57:34 +0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GioithieuComponent } from './gioithieu/gioithieu.component';
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { NewsComponent } from './news/news.component';
+// import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+// Kết hợp cả hai tuyến đường vào một mảng duy nhất
+const routes: Routes = [
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: GioithieuComponent},
+  { path: 'rooms', component:RoomsComponent},
+  { path: 'services', component:ServicesComponent},
+  { path: 'news', component:NewsComponent},
+  { path: 'contact', component:ContactComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], // Sử dụng mảng tuyến đường đã kết hợp
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomDetailComponent } from './room/room-detail/room-detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { GioithieuComponent } from './gioithieu/gioithieu.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { ServicesComponent } from './services/services.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    GioithieuComponent,
+    RoomsComponent,
+    ServicesComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
